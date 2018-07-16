6c5e0927a1a89db84b73aa9212878768e30cc902
andy23512 <andy23512@gmail.com>
2018-07-16 12:42:03 +0800

    init with ctare cli

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import 'semantic-ui-reset/reset.css'
+import '@/assets/global.sass'
+
+import Vue from 'vue'
+import axios from 'axios'
+
+import App from '@/App.vue'
+import store from '@/store'
+
+Vue.config.productionTip = false
+Vue.prototype.$http = axios
+
+new Vue({
+  store,
+  render: h => h(App),
+}).$mount('#app')
